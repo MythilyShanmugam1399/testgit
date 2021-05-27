66a2bc4332a3c7674fcee2b01a2e1f49b4387258
MythilyShanmugam1399 <mythilyshanmugam99@gmail.com>
2021-05-27 21:39:59 +0530

    Clone

---
+++ src/com/hcl/Main.java
@@ -0,0 +1,13 @@
+package com.hcl;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Initial");
+		System.out.println("Second commit");
+                System.out.println("Cloned Project");
+
+	}
+
+}
